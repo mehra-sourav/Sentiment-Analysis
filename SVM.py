21b15b49a0dc934c823aa8bcccc13b132bf374b6
LAPTOP-SOURAV <1997mehra@gmail.com>
2018-09-03 16:15:34 +0530

    Updated SVM.py

--- SVM.py
+++ SVM.py
@@ -1,1 +1,20 @@
 import sklearn
+
+#For 1000 review data
+#f=open('1000train.pkl','rb')
+#train=pkl.load(f)
+#f.close()
+
+#f=open('1000test.pkl','rb')
+#test=pkl.load(f)
+#f.close()
+
+#For 12500 review data
+f=open('12500train.pkl','rb')
+train=pkl.load(f)
+f.close()
+
+f=open('12500test.pkl','rb')
+test=pkl.load(f)
+f.close()
+
